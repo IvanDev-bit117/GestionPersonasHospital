1040f4dcb71ef75f29ae0a5d9defbe919631289d
Ivan Dev~ <jorgecastaneda.ia20@gmail.com>
2026-08-28 17:37:02 -0500

    Sexto commit, crecion metodo paciente

--- src/Paciente.java
+++ src/Paciente.java
@@ -2,4 +2,11 @@ public class Paciente extends Persona{
 
     public String enfermedad = "";
     public String afiliacion = "";
+
+    public void recibirtratamiento(){
+
+        System.out.println("\n ==Paciente==");
+        System.out.println("Nombre: "+ nombre);
+        System.out.println("Numero de documento: " + documento + "/ Edad: "+ edad);
+        System.out.println("Enfermedad: " + enfermedad + " / Tipo afiliacion: "+ afiliacion);
 }
